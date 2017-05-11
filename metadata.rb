319f94f49ee3f365c0eff342383a6e6b11dc8717
sutirthab <sutirtha_bhaumik@mcafee.com>
2017-05-11 17:46:56 +0530

    Add files via upload
    Added cookbook metadata file.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'nsp-aws-chef-cookbook'
+maintainer 'McAfee Inc.'
+license 'Apache 2.0'
+description 'Installs/Configures Network Security Platform Virtual Probes'
+long_description 'This cookbook installs Network Security Platform Virtual Probes on Debian-based (Debian, Ubuntu) and RHEL-based (RHEL, CentOS, Amazon Linux), Suse Linux and Windows operating systems.'
+version '0.1.0'
+
