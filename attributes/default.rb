dbd1946b55296ce27b2f5133a80c2f1a1892fa33
sutirthab <sutirtha_bhaumik@mcafee.com>
2017-05-11 17:56:23 +0530

    Create default.rb
    Added configurable attributes.

---
+++ attributes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: nsp-aws-chef-cookbook
+# Attributes:: default
+#
+################################################################################
+# Copyright (c) 2017 McAfee Inc. - All Rights Reserved.
+################################################################################
+
+#Modify NSM IP here
+default['nsp']['nsm_ip'] = '127.0.0.1'
+
+#Modify cluster name here
+default['nsp']['cluster_name'] = 'nsp-prod-us-west-1'
